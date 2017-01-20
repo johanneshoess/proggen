e6e52bd55e68c6814b45f22a1839476d05bb755a
Johannes Höß <johannes@hoess-ottersweier.de>
2017-01-20 13:03:26 +0100

    kontrollstrukturen

--- c/kram/kontStruck.c
+++ c/kram/kontStruck.c
@@ -5,12 +5,14 @@ void useFor(int k);
 void useWhile(int k);
 void useDoWhile(int k);
 void useIf(int k);
+void switchCase(int k);
 
 int main(int argc, char * argv[]){
     useFor(2);
     useWhile(3);
     useDoWhile(0);
     useIf(-1);
+    switchCase(3);
 }
 
 void useFor(int k){
@@ -38,5 +40,19 @@ void useDoWhile(int k){
 void useIf(int k){
     if(k){
         printf("print IF k is not 0\n");
+    }
+}
+
+void switchCase(int k){
+    switch(k){
+        case 1:
+            printf("hier wurde der erste CASE angesteuert\n");
+            break;
+        case 2:
+            printf("und der zweite CASE folgt angesteuert\n");
+            break;
+        case 3:
+            printf("hier der dritte CASE\n");
+            break;
     }
 }
