1143ebce2b602ea9e4bf83375e489075edd52639
Johannes Höß <johannes@hoess-ottersweier.de>
2017-01-20 12:25:24 +0100

    bitshift c kram

---
+++ c/kram/bit01.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+enum Scheine { Prog1 = 1<<0,
+                Prog2 = 1<<1,
+                Prog3 = 1<<2,
+                ADS = 1<<3,
+                Theo = 1<<4};
+
+void printschein(int s){
+    if(s & Prog1) printf("Programmieren1 ");
+    if(s & Prog2) printf("Programmieren2 ");
+    if(s & Prog3) printf("Programmieren3 ");
+    if(s & ADS) printf("AlgoUndDaten ");
+    if(s & Theo) printf("Theologie ");
+    printf("\n");
+}
+
+int main(int argc, char* argv[])
+{
+    
+    printf("%s, %s, %s\n%d\n", argv[1], argv[2], argv[3], argc);
+    
+    enum Scheine Susi = Prog1 | Prog2 | Prog3;
+    int Rudi = ADS | Theo | Prog3;
+
+    printschein(Susi);
+    printschein(Rudi);
+    printschein(Rudi | Susi);
+    printschein(Rudi&Susi);
+    printschein(~Susi);
+    return 0;
+}
+
+
