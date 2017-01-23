a666760188b3455007cb80d906d6b6ec1fbdb32d
Johannes Höß <johannes@hoess-ottersweier.de>
2017-01-23 17:33:22 +0100

    python und c beginner

---
+++ py/ueb05/a04.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+lis = range(0,100)
+print(lis)
+
+# die ersten 10 zahlen
+print(lis[:10])
+
+# die letzten zehn zahlen
+
+# jede 10. zahl (beginnend mit 0)
+
+# die mittlere zahl
+
+# jede dritte zahl außer von den ersten vier und den letzten fünf
+
+# jede dritte zahl aber davon nicht die ersten vier und nicht die letzten fünf
---
+++ py/ueb05/append.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+lis = [1,2,3,4,"fünf", 6]
+
+def append(lis, ele):
+    lis.append(ele) # wie kann man das selbst implementieren?
